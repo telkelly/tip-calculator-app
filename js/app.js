b152d2603416b7d30f33456944b62766072ccfa7
Kelly <84016657+telkelly@users.noreply.github.com>
2022-06-19 21:59:12 +0300

    set custom button tip

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const bill = document.getElementById("inp-bill");
 const tipBtn = document.querySelectorAll('.btn');
+const customBtn = document.getElementById("input-btn");
 
 
-let tipValue = 0.15;
 let billValue = 0.0;
+let tipValue = 0.15;
+let customBtnValue = 0;
 
 bill.addEventListener('input', setBillValue);
+
 tipBtn.forEach(btn => {
     btn.addEventListener('click', handleClick);
 })
+
+customBtn.addEventListener('input', setTipValue);
 
 //check for numbers
 function validateFloat(s) {
@@ -25,24 +30,37 @@ function setBillValue() {
   //exclude letters
   if (!validateFloat(bill.value)) {
     bill.value = bill.value.substring(0, bill.value.length - 1);
-    console.log(bill.value);
   }
 
   //return value as a floating point number
   billValue = parseFloat(bill.value);
 }
 
+function setTipValue() {
+    if (!validateFloat(customBtn.value)) {
+        customBtn.value = customBtn.value.substring(0, customBtn.value.length - 1);
+        console.log(customBtn.value)
+    }
+
+    tipValue = parseFloat(customBtn.value/100)
+    
+    tipBtn.forEach((btn) => {
+      btn.classList.remove("btn-active");
+    });
+}
+
 function handleClick(event) {
+
+    //clear active state
     tipBtn.forEach(btn => {
         btn.classList.remove('btn-active')
-        
+        //set active state
         if (event.target.innerHTML == btn.innerHTML) {
             btn.classList.add('btn-active');
             tipValue = parseFloat(btn.innerHTML) / 100;
         }
-    
-    
     });
 
-    console.log(tipValue)
+    //clear custom tip
+    customBtn.value = '';
 }
